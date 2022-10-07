d22859a608938927807b0973d8caa8ea9926297e
Vincent <113322585+PLM-Vincent@users.noreply.github.com>
2022-10-07 22:58:49 +0800

    Initial commit

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from Vincent Ferrer"; //letter subject
+	$to = 'vnferrer01@gmail.com'; // Enter Your E-mail
+	$from = 'admin@you-site-name.com'; // Admin e-mail
+	$fromName = 'Vincent Ferrer'; // Your company name
+	$charset = 'UTF-8';
+?>
